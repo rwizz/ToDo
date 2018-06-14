41d2ec4ba438ff48eb43d41fc96f6b7496aa8eec
Nico Scheidegger <nico.scheidegger@gibmit.ch>
2018-06-14 12:37:57 +0200

    task angepasst

--- ToDo/src/DTO/Task.java
+++ ToDo/src/DTO/Task.java
@@ -7,19 +7,57 @@ int id;
 String title;
 Timestamp TimeCreated;
 Boolean status;
-int TimeFinish;
+int day;
+int month;
+int year;
+int hour;
+int minute;
 Boolean favorite;
 	
 	public Task() {
 		
 	}
-	public Task(int id,String title,Timestamp TimeCreated,Boolean status,int TimeFinish,Boolean favorite) {
+	public Task(int id,String title,Timestamp TimeCreated,Boolean status,int day,int month,int year,int hour,int minute,Boolean favorite) {
 		setId(id);
 		setTitle(title);
 		setTimeCreated(TimeCreated);
 		setStatus(status);
-		setTimeFinish(TimeFinish);
+		setDay(day);
+		setMonth(month);
+		setYear(year);
+		setHour(hour);
+		setMinute(minute);
 		setFavorite(favorite);
+	}
+	public int getDay() {
+		return day;
+	}
+	public void setDay(int day) {
+		this.day = day;
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getHour() {
+		return hour;
+	}
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+	public int getMinute() {
+		return minute;
+	}
+	public void setMinute(int minute) {
+		this.minute = minute;
 	}
 	public int getId() {
 		return id;
@@ -44,12 +82,6 @@ Boolean favorite;
 	}
 	public void setStatus(Boolean status) {
 		this.status = status;
-	}
-	public int getTimeFinish() {
-		return TimeFinish;
-	}
-	public void setTimeFinish(int timeFinish) {
-		TimeFinish = timeFinish;
 	}
 	public Boolean getFavorite() {
 		return favorite;
